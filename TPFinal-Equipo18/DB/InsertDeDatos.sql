d146c1bb5c26a31e35998f251a283a69a7125d82
Joaquingigena <joagigena-11@hotmail.com>
2023-06-15 01:18:32 -0300

    Agrego datos a la DB

---
+++ TPFinal-Equipo18/DB/InsertDeDatos.sql
@@ -0,0 +1,20 @@
+
+select *from Bebidas
+select *from Marcas
+select *from Categorias
+
+--Insert de Marcas
+insert into Marcas values('Coca Cola',0)
+insert into Marcas values('Manaos',0)
+insert into Marcas values('Branca',0)
+insert into Marcas values('Termidor',0)
+
+--insert Categorias
+insert into Categorias values('Gaseosa')
+insert into Categorias values('Vino')
+insert into Categorias values('Aperitivo')
+
+--insert Bebidas
+insert into Bebidas values('A11','Fernet ',2000,'Bebida alcoholica elaborada con hierbas',1,1,'https://labebidadetusfiestas.com.ar/37266/fernet-branca-1lt.jpg',3,6)
+insert into Bebidas values('A22','Manao ',300,'Gaseosa berreta',1,1,'https://www.argensend.com/wp-content/uploads/2021/09/D_NQ_NP_716200-MLA43739181284_102020-F.webp',2,4)
+insert into Bebidas values('B33','Terminaitor ',500,'Jugo de estracto de uva',1,1,'https://s3.amazonaws.com/storage.wobiz.com/138/138495/images/Large/1557322653_c2314b5790046933a89ca07dbc544870.138495.jpeg',4,5)
