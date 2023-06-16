c1fdf83b1faadd2085d7646ab6df52383daf142e
Brian Galeano <108309802+CobyGaleano@users.noreply.github.com>
2023-06-15 22:22:05 -0300

    Agregado visual
    Se agrego un footer (falta darle cariño), carrito y formulario para log-in

---
+++ TPFinal-Equipo18/Carrito.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Carrito.aspx.cs" Inherits="TPFinal_Equipo18.Carrito" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>En esta pagina se mostraran los productos agregados al carrito </h2>
+</asp:Content>
---
+++ TPFinal-Equipo18/Carrito.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPFinal_Equipo18
+{
+
+
+    public partial class Carrito
+    {
+    }
+}
